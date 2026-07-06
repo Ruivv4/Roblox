7f5622faaba3638f6ce14583ad1701665c842c4b
Ruivv4 <prodnoee@gmail.com>
2026-07-06 18:55:18 -0400

    Add Rue Hub loader

---
+++ loader.lua
@@ -0,0 +1,6 @@
+getgenv().RueHubConfig = {
+    MainAccountsList = {"awsomedue1234"},
+    AltAccountsList = {"Eiraleelin"},
+}
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Ruivv4/Roblox/main/RueHub.lua"))()
