9f7c78b7f2009128e2fc21e41ca404e1d39aa081
Ruivv4 <prodnoee@gmail.com>
2026-07-06 23:38:27 -0400

    Add alt account name variants

--- loader.lua
+++ loader.lua
@@ -1,6 +1,6 @@
 getgenv().RueHubConfig = {
     MainAccountsList = {"awsomedue1234"},
-    AltAccountsList = {"Eiraleelin"},
+    AltAccountsList = {"Eiraleelin", "Eirlileen", "eirlileen"},
 }
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Ruivv4/Roblox/main/RueHub.lua"))()
